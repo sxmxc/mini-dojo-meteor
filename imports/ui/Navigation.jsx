51e1281696087d32fa5351e81b72843d50e5f0e2
sxmxc <steven.mcneely@live.com>
2017-02-07 19:54:48 -0600

    encapsulating nav in react component

--- imports/ui/Navigation.jsx
+++ imports/ui/Navigation.jsx
@@ -4,6 +4,52 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 class Navigation extends Component {
   render() {
-    return ();
+    return (
+      <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
+        <ul class="nav nav-pills flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="/dashboard/overview">Overview
+            </a>
+            <span class="sr-only">(current)</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Check-In/Check-Out</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Service Orders</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">TimeClock</a>
+          </li>
+        </ul>
+
+        <ul class="nav nav-pills flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Credit Cards</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Inventory</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Store Goals</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Accounting</a>
+          </li>
+        </ul>
+
+        <ul class="nav nav-pills flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="/dashboard/employees">Employees</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Text-A-Nerd</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Knowledge Base</a>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
